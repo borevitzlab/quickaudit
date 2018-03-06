343dbe50fee5cf0360067987b8ec0e3af5bb7693
Kevin Murray <kdmfoss@gmail.com>
2018-03-07 10:15:47 +1100

    Add sha1 hash, print errors to stderr

--- quickaudit.py
+++ quickaudit.py
@@ -3,11 +3,24 @@ from __future__ import division, print_function
 import zbarlight as zbar
 from PIL import Image
 import sys
+from sys import stderr, stdout, stdin
 from multiprocessing import Pool
 from os.path import getsize, splitext
 import os
 import numpy as np
 import rawpy
+import hashlib
+
+
+def hashfile(filepath):
+    blocksize = 2**2016
+    hasher = hashlib.sha1()
+    with open(filepath, 'rb') as fh:
+        buf = fh.read(blocksize)
+        while len(buf) > 0:
+            hasher.update(buf)
+            buf = fh.read(blocksize)
+    return hasher.hexdigest()
 
 
 def qrdecode(image):
@@ -34,30 +47,38 @@ def audit(imgpath):
     statusstr = "OK"
     qrstr = None
     meanpxstr = None
+    hashstr = None
+    try:
+        hashstr = hashfile(imgpath)
+    except Exception as e:
+        print(str(e), file=stderr)
+        hashstr = "HASHING_ERROR"
     try:
         img = load_image(imgpath)
-    except:
+    except Exception as e:
+        print(str(e), file=stderr)
         statusstr = "IMAGE_LOAD_ERROR"
         qrstr = meanpxstr = 'NA'
     if qrstr is None:
         try:
             qr = qrdecode(img)
             qrstr = ';'.join(sorted(qr)) if qr else 'NOT_DETECTED'
-        except:
+        except Exception as e:
+            print(str(e), file=stderr)
             qrstr = "QR_ERROR"
     if meanpxstr is None:
         meanpxstr = "{:0.2f}".format(meanpixel(img))
-    return (imgpath, fsize, statusstr, qrstr, meanpxstr)
+    return (imgpath, fsize, hashstr, statusstr, qrstr, meanpxstr)
 
 if __name__ == "__main__":
     nthreads = int(os.environ.get("PBS_NCPUS", 1))
     pool = Pool(nthreads)
-    print('file\tfile_size_bytes\tstatus\tqrcodes\tmean_pixel_value')
+    print('file\tfile_size_bytes\tsha1_hash\tstatus\tqrcodes\tmean_pixel_value')
 
     if len(sys.argv) > 1:
         files = sys.argv[1:]
     else:
-        print("Accepting input files from stdin, kill me if you don't want that", file=sys.stderr)
+        print("Accepting input files from stdin, kill me if you don't want that", file=stderr)
         files = map(lambda f: f.rstrip("\n"), sys.stdin)
 
     for res in pool.imap_unordered(audit, files):
